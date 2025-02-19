ca5adfe48092a1d07fca7fa16214ae47dc5df6a8
BookHaven12 <iyermegan@gmail.com>
2025-02-19 10:00:48 -0700

    adding query file

---
+++ EmployeeSQL/Query.sql
@@ -0,0 +1,28 @@
+-- SQL Query
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, salary
+FROM employees AS e
+LEFT JOIN salaries AS s
+ON e.emp_no = s.emp_no
+ORDER BY emp_no;
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31'
+ORDER BY hire_date;
+
+SELECT t.title, d.dept_no, d.dept_name, e.emp_no, last_name, first_name
+FROM titles AS t
+JOIN employees AS e
+ON t.title_id = e.emp_title_id
+JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+JOIN departments AS d
+ON dm.dept_no = d.dept_no;
+
+SELECT * 
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no;
