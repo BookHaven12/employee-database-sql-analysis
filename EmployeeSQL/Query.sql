087df039d7de70e7ac9218c21c2e20ae7932c501
BookHaven12 <iyermegan@gmail.com>
2025-02-20 10:01:35 -0700

    Updated search query

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -1,5 +1,3 @@
--- SQL Query
-
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, salary
 FROM employees AS e
 LEFT JOIN salaries AS s
@@ -20,9 +18,35 @@ ON e.emp_no = dm.emp_no
 JOIN departments AS d
 ON dm.dept_no = d.dept_no;
 
-SELECT * 
+SELECT de.dept_no, de.emp_no, last_name, first_name, dept_name
 FROM employees AS e
 JOIN dept_emp AS de
 ON e.emp_no = de.emp_no
 JOIN departments AS d
-ON de.dept_no = d.dept_no;
+ON de.dept_no = d.dept_no
+ORDER BY de.dept_no;
+
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+SELECT e.emp_no, last_name, first_name, dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
+
+SELECT e.emp_no, last_name, first_name, dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+SELECT last_name, COUNT(last_name) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
